d5b3c07f0d99a95935c5c507bddd08eab9b744fe
opensophy-projects <veilstack.it@gmail.com>
2026-02-01 12:59:35 +0700

    Updated HighlightText.tsx

---
+++ src/components/HighlightText.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { escapeRegExp } from '../lib/utils';
+
+interface HighlightTextProps {
+  text: string;
+  query: string;
+}
+
+export const HighlightText: React.FC<HighlightTextProps> = ({ text, query }) => {
+  if (!query) {
+    return <>{text}</>;
+  }
+
+  const escapedQuery = escapeRegExp(query);
+  const parts = text.split(new RegExp(`(${escapedQuery})`, 'gi'));
+
+  return (
+    <>
+      {parts.map((part, i) =>
+        part.toLowerCase() === query.toLowerCase() ? (
+          <mark
+            key={`hl-${i}`}
+            style={{
+              backgroundColor: 'rgb(59, 130, 246)',
+              color: 'white',
+              padding: '2px 4px',
+              borderRadius: '2px',
+              fontWeight: 600,
+            }}
+          >
+            {part}
+          </mark>
+        ) : (
+          <span key={`txt-${i}`}>{part}</span>
+        )
+      )}
+    </>
+  );
+};
