be98abcb9c1456bbcf51641477bc9a3a0c21a564
opensophy-projects <veilstack.it@gmail.com>
2026-02-01 12:56:42 +0700

    Updated table.ts

---
+++ src/types/table.ts
@@ -0,0 +1,17 @@
+export interface TableControlsState {
+  searchQuery: string;
+  sortColumn: number | null;
+  sortDirection: 'asc' | 'desc' | 'none';
+  filters: Map<number, Set<string>>;
+  visibleColumns: Set<number>;
+}
+
+export interface ParsedRow {
+  element: Element;
+  cells: string[];
+}
+
+export interface ParsedTable {
+  headers: string[];
+  rows: Element[];
+}
