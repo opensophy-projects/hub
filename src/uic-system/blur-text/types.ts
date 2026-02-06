55b6c00e4353cd17947c3a8af85c61d0c5672412
opensophy-projects <veilstack.it@gmail.com>
2026-02-06 11:55:08 +0700

    Updated types.ts

---
+++ src/uic-system/blur-text/types.ts
@@ -0,0 +1,16 @@
+import { Easing } from 'motion/react';
+
+export type BlurTextProps = {
+  text?: string;
+  delay?: number;
+  className?: string;
+  animateBy?: 'words' | 'letters';
+  direction?: 'top' | 'bottom';
+  threshold?: number;
+  rootMargin?: string;
+  animationFrom?: Record<string, string | number>;
+  animationTo?: Array<Record<string, string | number>>;
+  easing?: Easing | Easing[];
+  onAnimationComplete?: () => void;
+  stepDuration?: number;
+};
