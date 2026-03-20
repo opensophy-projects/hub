d4fd5dc07c210fd3b5b560484adbd3dcf9d76b2b
opensophy-projects <veilstack.it@gmail.com>
2026-03-20 22:39:32 +0700

    Updated contacts.ts

--- src/shared/data/contacts.ts
+++ src/shared/data/contacts.ts
@@ -9,7 +9,6 @@ export interface ContactItem {
 export const CONTACTS: ContactItem[] = [
   { href: 'https://opensophy.com/', title: 'Сайт', subtitle: 'opensophy.com', external: true },
   { href: 'mailto:opensophy@gmail.com', title: 'Email', subtitle: 'opensophy@gmail.com', external: false },
-  { href: 'https://t.me/veilosophy', title: 'Telegram', subtitle: '@veilosophy', external: true },
   { href: 'https://github.com/opensophy-projects', title: 'GitHub', subtitle: 'opensophy', external: true },
   { href: 'https://habr.com/ru/users/opensophy/', title: 'Habr', subtitle: 'opensophy', external: true },
   { href: 'https://www.reddit.com/user/opensophy/', title: 'Reddit', subtitle: 'opensophy', external: true },
