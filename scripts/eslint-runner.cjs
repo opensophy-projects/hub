8d0e713d5911875175afa17242c2731036399005
opensophy-projects <veilstack.it@gmail.com>
2026-05-27 13:05:26 +0700

    Updated eslint-runner.cjs

--- scripts/eslint-runner.cjs
+++ scripts/eslint-runner.cjs
@@ -3,7 +3,6 @@
 
 const Module = require("node:module");
 
-// Отключаем кеш компиляции, если метод доступен
 if (typeof Module.enableCompileCache === "function") {
   Module.enableCompileCache = () => ({ status: "DISABLED_BY_RUNNER" });
 }
