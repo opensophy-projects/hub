adfd6764458e3707987093022a9178c5e7b6c101
opensophy-projects <veilstack.it@gmail.com>
2026-06-16 14:22:53 +0700

    Updated contacts.ts

--- src/shared/data/contacts.ts
+++ src/shared/data/contacts.ts
@@ -6,8 +6,5 @@ export interface ContactItem {
 }
 
 export const CONTACTS: ContactItem[] = [
-  { href: 'https://opensophy.com/', title: 'Сайт', subtitle: 'opensophy.com', external: true },
-  { href: 'mailto:opensophy@gmail.com', title: 'Email', subtitle: 'opensophy@gmail.com', external: false },
-  { href: 'https://github.com/opensophy-projects', title: 'GitHub', subtitle: 'opensophy', external: true },
-  { href: 'https://habr.com/ru/users/opensophy/', title: 'Habr', subtitle: 'opensophy', external: true },
+
 ];
